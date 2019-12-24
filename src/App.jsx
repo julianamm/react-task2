b7731380c2fa81764421042a052c1043b559fea3
Juliana Morais <julianamm88@gmail.com>
2019-12-24 14:58:46 -0800

    create Char Component, added styling, rendered list of CharComponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
     state = {
@@ -11,6 +12,10 @@ class App extends Component {
         this.setState({ userInput: e.target.value });
     }
     render() {
+        const charList = this.state.userInput.split('').map(ch => {
+            return <Char character={ch} />
+        });
+
         return (
             <div>
                 <h1>Hello, world!</h1>
@@ -19,6 +24,7 @@ class App extends Component {
                     value={this.state.userInput}>
                 </input>
                 <Validation inputLength={this.state.userInput.length} />
+                {charList}
                 <p></p>
             </div>
         );
